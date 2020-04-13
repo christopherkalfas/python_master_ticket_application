62acbab944e4183fbbe560e3ab515de02d12abe0
Chris Kalfas <christopherkalfas22@gmail.com>
2020-04-13 16:35:21 -0400

    Welcome user to app and gather user name and total number of requested tickets and assigned those to variables. Calced the total cost and displayed it to user.

--- masterticket.py
+++ masterticket.py
@@ -1,3 +1,19 @@
 TICKET_PRICE = 10 
 
 tickets_ramaining = 100 
+
+# output how many tickets remaining using the tickets_remaining variable
+
+print("Number of remaining tickets: {}".format(tickets_ramaining))
+
+# Gather the user's name and assign it to a new variable
+
+user_name = input("What is your name: ")
+
+# prompt the user by name and ask how many tickets they would like
+number_of_tickets_wanted = int(input("Hello, {}! How many tickets would you like? (Price per ticket: ${}.00) ".format(user_name, TICKET_PRICE)))
+#Calc the price (number_of_ticket_wanted * the price) and assign to a variable
+total = (TICKET_PRICE * number_of_tickets_wanted)
+#outputthe price to the screen
+
+print("Total: ${}.00 for {} tickets ".format(total, number_of_tickets_wanted))
