8fdf6f6520729bad8fcb091b4c18d1f60e788247
Chris Kalfas <christopherkalfas22@gmail.com>
2020-04-14 12:47:38 -0400

    Refactored price total but defining a calculate_price method. Also incorporated, a new constant variable SERVICE_CHARGE which is in the calculate_price method.

--- masterticket.py
+++ masterticket.py
@@ -1,21 +1,25 @@
 TICKET_PRICE = 10 
+SERVICE_CHARGE = 2
 
 tickets_ramaining = 100 
+
+def calculate_price(number_of_tickets):
+    return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
+
 
 while tickets_ramaining >= 1:
         print("Number of remaining tickets: {}".format(tickets_ramaining))
         user_name = input("What is your name: ")
         number_of_tickets = input("Hello, {}! How many tickets would you like? (Price per ticket: ${}.00) ".format(user_name, TICKET_PRICE))
-        #Expect ValueError
         try:
             number_of_tickets = int(number_of_tickets)
             if number_of_tickets > tickets_ramaining:
                 raise ValueError("There are only {} tickets remaining".format(tickets_ramaining))
         except ValueError as err:
-            #Include the error text in the output
+
             print("Oh no! That is not a valid value. {}. Try again.".format(err))
         else:
-            total = (TICKET_PRICE * number_of_tickets)
+            total = calculate_price(number_of_tickets)
             print("Total: ${}.00 for {} tickets ".format(total, number_of_tickets))
             confirmation = input("Are you sure you want to proceed with purchase of {} tickets for a total of ${}.00 USD? Y/N ".format(number_of_tickets, total ))
             if confirmation.upper() == "Y":
